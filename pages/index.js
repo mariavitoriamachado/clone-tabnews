a43f5111dd979e6d8c9babc407d6a91a239cf232
Maria Vitória <mariavitoriasilvast@gmail.com>
2023-09-14 22:36:25 +0000

    Adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Filipe é incrível! Muito obrigado por esta oportunidade e o seu conteúdo é realmente sensacional.</h1>
+  return (
+    <h1>
+      Filipe é incrível! Muito obrigado por esta oportunidade e o seu conteúdo é
+      realmente sensacional.
+    </h1>
+  );
 }
-
- export default Home;
+export default Home;
